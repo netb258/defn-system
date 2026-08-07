eb0827eebe01e9d94c0db1cad1ddcea63bff1cf1
netb258 <netb258@abv.bg>
2026-08-08 02:32:23 +0300

    Refactor VRAM address handling and add helpers

--- src/z80/vdp.clj
+++ src/z80/vdp.clj
@@ -1,4 +1,5 @@
 (ns z80.vdp
+  (:require [z80.memory :as memory])
   (:import [com.codingrodent.microprocessor.Z80 Z80Core]))
 
 (defrecord VdpState [
@@ -56,10 +57,23 @@
         h-val (quot (* line-cycles 3) 2)]
     (if (<= h-val 225)
       h-val
-      (bit-and (+ 202 (- h-val 226)) 0xFF))))
+      (memory/signed->unsigned (+ 202 (- h-val 226))))))
 
-;; NOTE: IN the following code, you will see this a lot (bit-and some-vram-address 0x3FFF).
+;; NOTE: The code needed a lot of this: (bit-and some-vram-address 0x3FFF).
 ;; The reason for this is that the vram pointer is 14 bits long and doesn't fit neatly into standard bytes and words (8 and 16 bits).
+;; In the end I just rolled it into a small helper function:
+
+(defn- take-14-bits
+  "Takes a Clojure number (which is stored as a long).
+  Returns a new number with only the first 14 bits kept as they are.
+  The rest of the bits are set to 0."
+  [number]
+  (bit-and number 2r0011111111111111))
+
+(defn- make-word 
+  "Takes two bytes and glues them together into a word."
+  [byte1 byte2]
+  (bit-or byte2 (bit-shift-left byte1 8)))
 
 (defn data-write! [^VdpState vdp ^long value]
   (let [op (int (.operation vdp))
@@ -68,7 +82,7 @@
       ;; VRAM Write
       ;; Even though the docs say that only Mode 1 is VRAM write, the actual hardware behaves like this.
       (or (= op 0) (= op 1) (= op 2))
-      (let [address (bit-and loc 0x3FFF)
+      (let [address (take-14-bits loc)
             ^bytes vram (.vram vdp)]
         (aset vram address (unchecked-byte value)))
 
@@ -80,21 +94,21 @@
 
     (-> vdp
         ;; Address pointer must wrap around at 14 bits (0x3FFF)
-        (assoc :vram-pointer (bit-and (inc loc) 0x3FFF))
+        (assoc :vram-pointer (take-14-bits (inc loc)))
         ;; FIX for FluBBa VDP test 9: Hardware writes to the data port explicitly overwrite the read buffer!
         (assoc :read-buffer value)
         (assoc :first-byte? true))))
 
 (defn data-read! [^VdpState vdp]
   (let [loc (int (.vram-pointer vdp))
-        address (bit-and loc 0x3FFF)
+        address (take-14-bits loc)
         ^bytes vram-arr (.vram vdp)
         ;; 1. The CPU receives what was ALREADY sitting in the hardware buffer
-        return-val (bit-and (int (.read-buffer vdp)) 0xFF)
+        return-val (memory/signed->unsigned (int (.read-buffer vdp)))
         ;; 2. Prefetch the NEXT byte from VRAM into the buffer for the next read
-        next-buffered-val (bit-and (aget vram-arr address) 0xFF)
+        next-buffered-val (memory/signed->unsigned (aget vram-arr address))
         ;; 3. Increment and wrap the VRAM address pointer
-        next-loc (bit-and (inc loc) 0x3FFF)]
+        next-loc (take-14-bits (inc loc))]
     [return-val (assoc vdp 
                        :vram-pointer next-loc 
                        :read-buffer next-buffered-val
@@ -109,15 +123,19 @@
 (defn control-write! [^VdpState vdp ^long value]
   (if (:first-byte? vdp)
     ;; First byte: Save and wait for the second byte
-    (let [clean-val (bit-and value 0xFF)
+    ;; When a first-byte arrives, we need to construct a new vram-pointer
+    ;; by keeping 6 bits from the old one and adding the 8 bits from the new first-byte.
+    (let [clean-val (memory/signed->unsigned value)
           old-loc (int (.vram-pointer vdp))
-          new-loc (bit-or (bit-and old-loc 0x3F00) clean-val)
+          ;; This value tells you everyting: 2r11111100000000.
+          ;; We keep the 1 bits as they currently are and set the rest to 0. Then we fill the 0 bits with clean-val.
+          new-loc (bit-or (bit-and old-loc 2r11111100000000) clean-val)
           
           ;; Hardware Prefetch: If currently in Read Mode (op 0), update read-buffer instantly!
           op (int (.operation vdp))
           ^bytes vram-arr (.vram vdp)
           updated-buffer (if (= op 0) 
-                           (bit-and (aget vram-arr (bit-and new-loc 0x3FFF)) 0xFF) 
+                           (memory/signed->unsigned (aget vram-arr (take-14-bits new-loc))) 
                            (int (.read-buffer vdp)))]
       (assoc vdp 
              :command-byte clean-val 
@@ -126,17 +144,19 @@
              :first-byte? false))
 
     ;; Second byte received: Combine both to process command
-    (let [low-byte (bit-and (:command-byte vdp) 0xFF)
-          high-byte (bit-and value 0xFF)
+    (let [low-byte (memory/signed->unsigned (:command-byte vdp))
+          high-byte (memory/signed->unsigned value)
           ;; Extract Operation Code (Top 2 bits of the second byte)
-          code-type (bit-shift-right (bit-and high-byte 0xC0) 6)
+          ;; We need to push these 2 bits all the way to the right to get a small number (0-3)
+          ;; Otherwise it works out to a large number (ex. 2r11000000 is 192)
+          code-type (bit-shift-right (bit-and high-byte 2r11000000) 6)
           ;; Extract Address (Lower 6 bits of high byte + full low byte)
-          new-loc (bit-or low-byte (bit-shift-left (bit-and high-byte 0x3F) 8))]
+          new-loc (make-word (bit-and high-byte 2r00111111) low-byte)]
       (cond
         ;; Mode 0: VRAM Read
         (= code-type 0)
         (let [^bytes vram-arr (.vram vdp)
-              buffered-val (if (< new-loc (count vram-arr)) (bit-and (aget vram-arr new-loc) 0xFF) 0)]
+              buffered-val (if (< new-loc (count vram-arr)) (memory/signed->unsigned (aget vram-arr new-loc)) 0)]
           (assoc vdp :vram-pointer (inc new-loc) :operation code-type :read-buffer buffered-val :first-byte? true))
 
         ;; Mode 1: VRAM Write
@@ -144,10 +164,9 @@
 
         ;; Mode 2: VDP Register Write (Top bits are 10xx xxxx)
         (= code-type 2)
-        (let [reg-num (bit-and high-byte 0x0F) 
+        (let [reg-num (bit-and high-byte 2r00001111) ;; Figure out exactly which register to write.
               ^ints regs-arr (.regs vdp)]
-          (when (< reg-num (alength regs-arr))
-            (aset regs-arr reg-num (int low-byte)))
+          (aset regs-arr reg-num (int low-byte))
           (assoc vdp :first-byte? true))
 
         ;; Mode 3: CRAM Pointer Setup (Top bits are 11xx xxxx)
