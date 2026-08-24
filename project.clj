f9cd626a4eac4e086bde4b1fafbe88e129a4d83a
netb258 <netb258@abv.bg>
2026-08-24 15:39:35 +0300

    Update Z80Processor version in project.clj

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   ;; :plugins [[cider/cider-nrepl "0.49.0"]]
-  :resource-paths ["local-libs/Z80Processor-4.0.0.jar"]
+  :resource-paths ["local-libs/Z80Processor-2.0.2.jar"]
   :dependencies [[org.clojure/clojure "1.11.3"]
                  [quil "2.7.1"]]
-                 ;; [com.codingrodent.microprocessor/Z80Processor "4.2.0"]
+                 ;; [com.codingrodent.microprocessor/Z80Processor "2.0.2"]
   :main ^:skip-aot z80.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
